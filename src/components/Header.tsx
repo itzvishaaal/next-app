1b1e5e6c2c6f50260f80e6bf7ec8600a6a29c64c
vishal singh <vishaliilm1996@gmail.com>
2023-10-09 12:46:23 +0530

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useTheme } from "next-themes";
+
+const Header = () => {
+  //   const [theme, setTheme] = useTheme();
+
+  return <div>Header</div>;
+};
+
+export default Header;
